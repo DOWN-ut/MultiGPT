d39645149f4ec9c3cb1a7c07a9bd42e103a9bfe7
Yahnis Saint-Val <yahnis.saint-val@etu.umontpellier.fr>
2023-09-15 18:03:20 +0200

    Added key.txt reading and GptAgent class to manager conversations easilly

--- gpt.py
+++ gpt.py
@@ -1,32 +1,72 @@
 import openai
+from colorama import Fore, Back, Style
 
-openai.api_key = "" #open key.txt
+max_response_tokens = 100
+
+
+keyFile = open("key.txt", "r")
+openai.api_key = keyFile.read() #open key.txt
 
 
 def gptRequest(context,request):
     context.append(request)
     chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=context,temperature = 1
+            model="gpt-3.5-turbo", 
+            messages=context,
+            temperature = 1,
+            max_tokens=max_response_tokens
         )
     choices = chat.choices
     reply = choices[0].message.content
     context.append({"role":"assistant","content":reply})
-    return reply
+    return context
 
-def displayContexts(context):
-    for i in range(0,len(context)):
-        print(context[i]["content"])
+def displayContext(context):
+    color = Fore.CYAN
+    if context["role"] == "user":
+        color = Fore.YELLOW
+    elif context["role"] == "system":
+        color = Fore.GREEN
+    print(color + "> " + context["content"])
 
-def displayContext(context,time,count):
+def displayContexts(context,time,count):
     for i in range(0,count):
-        print(context[time + i]["content"])
+        displayContext(context[time + i])
+        print("   ")
+    print(Fore.WHITE)
 
-instructions = "You are Adele, a dumb student, studying computer science. You only talk using programming words."
+def makeRequest(text):
+    return {"role":"user", "content":text}
 
-time = 0
-context = [{"role": "system", "content": instructions}]
-request = {"role":"user", "content":"Hello, who are you ?"}
+def makePrePrompt(text):
+    return {"role":"system", "content":text}
 
-reply = gptRequest(context,request)
+class GptAgent:
+    def __init__(self, name,prepromtPath):
+        self.name = name
+        self.prepromt = makePrePrompt(open(prepromtPath, "r").read())
+        self.context = [self.prepromt]
 
-displayContext(context,0,3)
+    def tell(self,request):
+        self.context = gptRequest(self.context,makeRequest(request))
+        return self.context
+
+print("----------------------------")
+
+adele = GptAgent("Adele","agent_preprompt.txt")
+adeleContext = adele.tell("Hello, who are you ?")
+displayContexts(adeleContext,0,3)
+
+def test():
+    time = 0
+    context = [{"role": "system", "content": instructions}]
+
+    request = makeRequest("Hello, who are you ?")
+    reply = gptRequest(context,request)
+    displayContexts(context,0,3)
+
+    request = makeRequest("No you are not.")
+    reply = gptRequest(context,request)
+    displayContexts(context,3,2)
+
+    print(Fore.WHITE)
