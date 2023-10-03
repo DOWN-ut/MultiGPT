c03246f5e120fbc687ddcfbe459b6ade30bf1a7e
Léa <leaserrano1119@gmail.com>
2023-10-03 13:35:20 +0200

    Amélioration de l'interface

--- test_pygame.py
+++ test_pygame.py
@@ -1,71 +1,120 @@
 import tkinter as tk
 from tkinter import scrolledtext
+from tkinter import PhotoImage
 import math
+import random
 
 # Données de la conversation
 messagesList = [
     {"name": "Normi", "text": "Coucou je suis un loup garou"},
-    {"name": "Adèle", "text": "Salut ! Je suis voyante"}
+    {"name": "Adèle", "text": "Salut ! Je suis voyante aaaaaaaaaaaaaaaaaa"}
 ]
 
-# Fonction pour dessiner les cercles et les noms
-def draw_circles_with_names(canvas):
-    num_circles = 6
+agents = [
+	{"name": "Bob", "image_path": "images/werewolf.png"}, 
+    {"name": "Alice", "image_path": "images/werewolf.png"},
+    {"name": "Adele", "image_path": "images/werewolf.png"},
+    {"name": "Yahnis", "image_path": "images/werewolf.png"},
+    {"name": "Léa", "image_path": "images/werewolf.png"},
+    {"name": "Normi", "image_path": "images/werewolf.png"}
+]
+
+num_circles = 6
+random_color = []
+
+for _ in range(num_circles):
+    while True:
+        # Générer des composants de couleur RVB aléatoires
+        red = random.randint(100, 255)
+        green = random.randint(100, 255)
+        blue = random.randint(100, 255)
+        
+        # Vérifier si la couleur est suffisamment lumineuse (non foncée)
+        if (red + green + blue) / 3 > 150:  # Exemple de seuil de luminosité
+            break
+
+    # Ajouter la couleur générée à la liste
+    random_color.append("#{:02X}{:02X}{:02X}".format(red, green, blue))
+
+# Créez une liste pour stocker les objets PhotoImage
+image_list = []
+
+# Modifiez la fonction pour dessiner les cercles et afficher les images
+def draw_circles_with_names_and_images(canvas):
     circle_radius = 60
-    circle_center_x = 300
+    circle_center_x = 400
     circle_center_y = 300
-    circle_spacing = 100
+    circle_spacing = 150
 
     # Dessiner les cercles
     for i in range(num_circles):
         angle = 2 * math.pi * i / num_circles
         x = circle_center_x + (circle_radius + circle_spacing) * math.cos(angle)
         y = circle_center_y + (circle_radius + circle_spacing) * math.sin(angle)
-        canvas.create_oval(x - circle_radius, y - circle_radius, x + circle_radius, y + circle_radius, fill="red")
+        canvas.create_oval(x - circle_radius, y - circle_radius, x + circle_radius, y + circle_radius, fill=random_color[i])
 
         # Ajouter les noms au-dessus des cercles
-        name = f"Joueur {i+1}"
+        element = agents[i]
+        name = element["name"]
         canvas.create_text(x, y, text=name)
 
-# Créer une fenêtre
-fenetre = tk.Tk()
-fenetre.title("Chat Fenêtre")
+        # Ajouter les images en dessous des cercles
+        image_path = element.get("image_path")  # Obtenir le chemin de l'image
+        if image_path:
+            image = PhotoImage(file=image_path)  # Charger l'image
+            image = image.subsample(6, 6)
+            image_list.append(image)  # Ajouter l'image à la liste
+            canvas.create_image(x, y + circle_radius + 10, anchor=tk.N, image=image)
 
-# Réduire la largeur de la fenêtre avec le texte
-fenetre.geometry("1000x600")  # Largeur x Hauteur
 
-# Diviser la fenêtre en deux parties (gauche et droite)
-left_frame = tk.Frame(fenetre)
-right_frame = tk.Frame(fenetre)
+        # Ajouter les noms au-dessus des cercles
+        element = agents[i]
+        name = element["name"]
+        canvas.create_text(x, y, text=name)
 
-fenetre.configure(bg="white")
+def display_game() :
 
-left_frame.pack(side=tk.LEFT, padx=10)
-right_frame.pack(side=tk.RIGHT, padx=10, fill=tk.BOTH, expand=True)
+    # Créer une fenêtre
+    fenetre = tk.Tk()
+    fenetre.title("Chat Fenêtre")
 
-# Créer un canevas pour dessiner les cercles dans la partie gauche
-canvas = tk.Canvas(left_frame, width=600, height=600)
-canvas.configure(bg="white")
-canvas.pack()
-draw_circles_with_names(canvas)
+    # Réduire la largeur de la fenêtre avec le texte
+    fenetre.geometry("1200x700")  # Largeur x Hauteur
 
-# Créer un widget Texte défilant pour la conversation dans la partie droite
-conversation = scrolledtext.ScrolledText(right_frame, wrap=tk.WORD, state=tk.DISABLED, width=30)
-conversation.pack(fill=tk.BOTH, expand=True)
-conversation.configure(bg="#262626")
-conversation.tag_configure("user", foreground="blue")
+    # Diviser la fenêtre en deux parties (gauche et droite)
+    left_frame = tk.Frame(fenetre)
+    right_frame = tk.Frame(fenetre)
 
-# Parcourir la liste de messages et les afficher
-for message_data in messagesList:
-    name = message_data["name"]
-    message = message_data["text"]
-    conversation.config(state=tk.NORMAL)  # Activer l'édition
-    conversation.insert(tk.END, f" {name} : {message}\n", "user")
-    conversation.config(state=tk.DISABLED)  # Désactiver l'édition
-    conversation.yview(tk.END)  # Faire défiler vers le bas
+    fenetre.configure(bg="white")
 
-# Démarrer la fenêtre principale
-fenetre.mainloop()
+    left_frame.pack(side=tk.LEFT, padx=10)
+    right_frame.pack(side=tk.RIGHT, padx=10, fill=tk.BOTH, expand=True)
+
+    # Créer un canevas pour dessiner les cercles dans la partie gauche
+    canvas = tk.Canvas(left_frame, width=800, height=700)
+    canvas.configure(bg="white")
+    canvas.pack()
+    draw_circles_with_names_and_images(canvas)
+
+    # Créer un widget Texte défilant pour la conversation dans la partie droite
+    conversation = scrolledtext.ScrolledText(right_frame, wrap=tk.WORD, state=tk.DISABLED, width=30)
+    conversation.pack(fill=tk.BOTH, expand=True)
+    conversation.configure(bg="#262626")
+    conversation.tag_configure("user", foreground="blue")
+
+    # Parcourir la liste de messages et les afficher
+    for message_data in messagesList:
+        name = message_data["name"]
+        message = message_data["text"]
+        conversation.config(state=tk.NORMAL)  # Activer l'édition
+        conversation.insert(tk.END, f" {name} : {message}\n", "user")
+        conversation.config(state=tk.DISABLED)  # Désactiver l'édition
+        conversation.yview(tk.END)  # Faire défiler vers le bas
+
+    # Démarrer la fenêtre principale
+    fenetre.mainloop()
+
+display_game()
 
 
 
